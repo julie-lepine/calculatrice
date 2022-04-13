bb58085f79fa2105328e6029194acb7718270bf2
July <lepine.julie@live.fr>
2022-04-13 19:15:23 +0200

    Bugfix link JS

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 /* CALCULATRICE */
-const a = document.getElementById("num1").value;
-const b = document.getElementById("num2").value;
-const output = document.getElementById("output");
-
 function addition() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
     let sum = parseInt(a) + parseInt(b);
+    let output = document.getElementById("output");
     output.innerHTML = "La réponse est: <b>" + sum + "</b>";
 }
 
 function soustraction() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
     let sum = parseInt(a) - parseInt(b);
+    let output = document.getElementById("output");
     output.innerHTML = "La réponse est: <b>" + sum + "</b>";
 }
 function multiplication() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
     let sum = parseInt(a) * parseInt(b);
+    let output = document.getElementById("output");
     output.innerHTML = "La réponse est: <b>" + sum + "</b>";
 }
 
 function division() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
     let sum = parseInt(a) / parseInt(b);
+    let output = document.getElementById("output");
     output.innerHTML = "La réponse est: <b>" + sum + "</b>";
 }
 
 function modulo() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
     let sum = a % b;
+    let output = document.getElementById("output");
     output.innerHTML = "La réponse est: <b>" + sum + "</b>";
 }
 
